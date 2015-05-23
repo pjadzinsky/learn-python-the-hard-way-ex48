5c5ac9d3e68c49fbf9d124f7f0dceab4c1c397e2
Pablo Jadzinsky <jadz@stanford.edu>
2015-05-23 08:18:14 -0700

    Fixed the problem I had in my parser_tests file Apparently using pdb explicitly causes nosetests to fail

--- tests/parser_tests.py
+++ tests/parser_tests.py
@@ -1,19 +1,29 @@
 from nose.tools import *
 from ex48.parser import *
 from ex48.lexicon import *
-import pdb
 
 def test_corret_sentences():
-    pdb.set_trace()
-    #parse_sentence([('verb', 'go'), ('direction', 'north')])
+    # test a simple sentence 1
+    sentence = "go north"
+    scanned = scan(sentence)
+    parse_sentence([('verb', 'go'), ('direction', 'north')])
     sentence1 = parse_sentence(scan("go north"))
-    #print(sent_obj1.__dict__)
-    #scanned = scan(sentence)
-    #sent_obj1 = Sentence(scanned['noun'], scanned['verb'], scanned['object'])
-
+    
     sentence2 = Sentence((0,'a'), (1,'a'), (2,'a'))
     sentence2.object = 'north'
     sentence2.verb = 'go'
+    sentence2.subject = 'player'
+
+    assert_equal(sentence1.object, sentence2.object)
+    
+
+    # test a simple sentence 2
+    sentence = "player eat bear"
+    sentence1 = parse_sentence(scan(sentence))
+    
+    sentence2 = Sentence((0,'a'), (1,'a'), (2,'a'))
+    sentence2.object = 'bear'
+    sentence2.verb = 'eat'
     sentence2.subject = 'player'
 
     assert_equal(sentence1.object, sentence2.object)
